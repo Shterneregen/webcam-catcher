d7322828231dbf2f2e3450e7c73ca5f97e4ebff6
shterneregen <shterneregen@gmail.com>
2018-11-26 09:36:06 +0300

    181126; fix bat for stream

---
+++ test/openBrowser.bat
@@ -0,0 +1,4 @@
+@echo off
+set port=8989
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:%port%"
+pause
--- test/stream.bat
+++ test/stream.bat
@@ -1,2 +1,4 @@
+@echo off
+set port=8989
 java -jar WebViewer.jar -stream
 pause
